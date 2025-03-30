5a42e7dea102d19f197f0b4632128518898df0fa
tylryan <tlrrn5@gmail.com>
2025-03-30 13:28:27 -0500

    updated conversion-factorial.py

--- algo-practice/recursion/04-conversion-factorial.py
+++ algo-practice/recursion/04-conversion-factorial.py
@@ -8,33 +8,43 @@ def rfact(number):
     if number == 1:
         return number
 
+    print("BEFORE: ", number)
     res = rfact(number - 1)
+    print(f"AFTER: {number} * {res} = {number * res}")
 
     return res * number
 
 
+# His example shows that it can be done, my example shows the exact
+# translation (At least I think).
 def ifact(number):
 
-    s = []
+    stack = []
 
     while number > 0:
         print("BEFORE: ", number)
-        s.append(number)
+        stack.append(number)
         number -=1
 
     print("BASE CASE (JUST PRETEND)")
-    print("STACK: ", s)
+    print("STACK: ", stack)
 
     res = 1
 
-    for n in s:
+    # Just going backwards through the stack
+    i = len(stack)
+    while i > 0:
+        n = stack.pop()
         print(f"AFTER: {n} * {res} = {n * res}")
-        res *=n
+        res *= n
+
+        i-=1
 
     return res
 
     
 
 
-print(ifact(5))
+print("RFACT"); print(rfact(5))
+print("IFACT"); print(ifact(5))
 
