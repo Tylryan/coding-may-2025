bc2172d1c7fc623ad6c219b288d80df9085e8b79
tylryan <tlrrn5@gmail.com>
2025-04-07 12:30:07 -0500

    environment updated to allow assignment

--- jLox/NOTES.md
+++ jLox/NOTES.md
@@ -72,3 +72,54 @@ reader. Another way is precedence climbing which I believe is explained
 in part III.
 
 
+# Chapter 8
+## Assignment
+### Grammar
+```
+<assign> ::= IDENT "=" <expr>
+           | <next_precedence_level>
+```
+### Examples
+Below are two valid assignment expressions:
+```
+a.b.c = d;
+a = b = c = d;
+```
+
+Here is an invalid assignment where the left side is not
+an l-value.
+```
+"hello, world!" = 5;
+```
+### Implementation
+1. Evaluate the left side of the expression.
+2. While at an equal sign, parse the right side using the **right**
+   **associative** rule. 
+   - See below for the difference between right and left associativity in code.
+3. If the left side is an l-value (such as a Variable), assign the the right expression to the variable.
+    - If it is not, throw error.
+
+
+### Right Associativity
+Assignment is right associative. This means an expression like
+`a = b = c;` would be evaluated like `(a = (b = c));` instead of
+`((a = b) = c)`.
+
+In order to achieve right associativity, you would pretty much
+do the same thing as what you'd do for left associativity except
+for one thing: when parsing the right side, don't go up a precedence
+level.
+
+```python
+def assignment():
+    left = next_precedence_level()
+    
+    while match(EQUAL):
+        operator = previous()
+        right = assignment()
+        # If we were to instead go to the next precedence level
+        # here, then this would be left associative.
+        # right = next_precedence_level()
+        left = BinOp(left, operator, right)
+    return left
+```
--- jLox/src/Environment.java
+++ jLox/src/Environment.java
@@ -2,28 +2,59 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+    // Each time a new environment is created, it starts off
+    // with nothing in it.
     private final Map<String, Object> values = new HashMap<>();
+    final Environment enclosing;
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
 
     void define(String name, Object value) {
+        // Currently allows redeclaration of same variable.
         values.put(name, value);
     }
 
     void assign(Token name, Object value) {
+        // If the current scope contains the variable,
+        // assign the value to that variable.
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
             return;
         }
 
+        // Try assigning it to the parent environment
+        // recursively.
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+        }
+
+        // If the variable was not found in any scopes,
+        // throw an error.
         throw new RuntimeError(name,
                 String.format("Undefined variable '%s'.", name.lexeme));
     }
 
     Object get(Token name) {
+        // Check the current scope/environment for the
+        // variable.
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
 
+        // As long as the scope has a parent,
+        // recursively try to find the value in
+        // the parent environments.
+        if (enclosing != null)
+            return enclosing.get(name);
+
+        // If variable was never found, throw an error.
         throw new RuntimeError(name, String.format("Undefined variable '%s'.", name.lexeme));
     }
 }
