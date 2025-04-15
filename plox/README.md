ada47e8446b019a32fc69040d5d0e09a490a7eb2
tylryan <tlrrn5@gmail.com>
2025-04-15 11:16:09 -0500

    retrying to implement plox...

--- plox/README.md
+++
@@ -1,3 +0,0 @@
-# pLox
-My goal is to convert jLox into pLox and use a simpler design pattern. Why complicate
-things?
--- plox/lruntime_error.py
+++
@@ -1,8 +0,0 @@
-
-class LRuntimeError(RuntimeError):
-    token: object
-    message: str
-
-    def __init__(self, tok, message):
-        self.message = message
-        self.token = tok
