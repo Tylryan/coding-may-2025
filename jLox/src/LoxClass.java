408e1e517dd54f9fcb0d21242ac874d4c0e3f82c
tylryan <tlrrn5@gmail.com>
2025-04-23 11:40:02 -0500

    jlox: classes now have basic functions. Basically function pointers stored inside an instance/class

--- jLox/src/LoxClass.java
+++ jLox/src/LoxClass.java
@@ -1,10 +1,22 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LoxClass implements LoxCallable {
     final String name;
+    final Map<String, LoxFunction> methods;
 
-    LoxClass(String name){
+    LoxClass(String name, Map<String, LoxFunction> methods){
         this.name = name;
+        this.methods = methods;
+    }
+
+    LoxFunction findMethod(String name) {
+        if (methods.containsKey(name)) {
+            return methods.get(name);
+        }
+
+        return null;
     }
 
     @Override
