ca7ed62b9c04a68b1995d487cb2c21fa3ae4fe01
tylryan <tlrrn5@gmail.com>
2025-04-28 12:06:06 -0500

    eplox: stmts, exprs, and their unit tests complete

---
+++ eplox/stmt.py
@@ -0,0 +1,136 @@
+from exprs import Expr, expr_to_str
+from tokens import Token
+
+class Stmt:
+    kind: str = "Stmt"
+
+# Expression
+def expression_init(expression: Expr) -> Stmt:
+    assert isinstance(expression, Expr)
+    stmt = Stmt()
+    stmt.kind = "Expression"
+    stmt.expression = expression
+    return stmt
+
+def is_expression(stmt: object) -> bool:
+    try: return stmt.kind == "Expression"
+    except AttributeError: return False
+
+def expression_to_str(expression: Stmt) -> str:
+    expr = stmt_to_str(expression)
+    return f"Expression({expr})"
+
+# Return
+def return_init(keyword: Token, value: Expr) -> Stmt:
+    assert isinstance(keyword, Token)
+    assert isinstance(value, Expr)
+    stmt = Stmt()
+    stmt.kind = "Return"
+    stmt.keyword = keyword
+    stmt.value = value
+    return stmt
+
+def is_return(stmt: object) -> bool:
+    try: return stmt.kind == "Return"
+    except AttributeError: return False
+
+def return_to_str(ret: Stmt) -> str:
+    value: str = expr_to_str(ret.value)
+    return f"Return({value})"
+
+# Function
+def function_init(name: Token, params: list[Token], body: list[Stmt]) -> Stmt:
+    assert isinstance(name, Token)
+    assert isinstance(params, list[Token])
+    assert isinstance(body, list[Stmt])
+
+    stmt = Stmt()
+    stmt.kind = "Function"
+    stmt.name =  name
+    stmt.params = params
+    stmt.body =  body
+    return stmt
+
+def is_function(stmt: object) -> bool:
+    try: return stmt.kind == "Function"
+    except AttributeError: return False
+
+def function_to_str(ret: Stmt) -> str:
+    name: str = ret.name.lexeme
+    return f"Function({name})"
+
+# While
+def while_init(condition: Expr, body: Stmt) -> Stmt:
+    assert isinstance(condition, Expr)
+    assert isinstance(body, list[Stmt])
+
+    stmt = Stmt()
+    stmt.kind      = "While"
+    stmt.condition =  condition
+    stmt.body      = body
+    return stmt
+
+def is_while(stmt: object) -> bool:
+    try: return stmt.kind == "While"
+    except AttributeError: return False
+
+def while_to_str(ret: Stmt) -> str:
+    name: str = ret.name.lexeme
+    return f"While()"
+
+# If
+def if_init(condition: Expr, thenBranch: Stmt, elseBranch: Stmt) -> Stmt:
+    assert isinstance(condition, Expr)
+    assert isinstance(thenBranch, Stmt)
+    assert isinstance(elseBranch, Stmt | None)
+
+    stmt = Stmt()
+    stmt.kind       = "If"
+    stmt.condition  =  condition
+    stmt.thenBranch = thenBranch
+    stmt.elseBranch = thenBranch
+    return stmt
+
+def is_if(stmt: object) -> bool:
+    try: return stmt.kind == "If"
+    except AttributeError: return False
+
+def if_to_str(ret: Stmt) -> str:
+    name: str = ret.name.lexeme
+    return f"If()"
+# Block
+def block_init(statements: list[Stmt]) -> Stmt:
+    assert isinstance(statements, list[Stmt])
+
+    stmt = Stmt()
+    stmt.kind       = "Block"
+    stmt.statements  =  statements
+    return stmt
+
+def is_block(stmt: object) -> bool:
+    try: return stmt.kind == "Block"
+    except AttributeError: return False
+
+def block_to_str(block: Stmt) -> str:
+    name: str = block.name.lexeme
+    return f"Block()"
+
+# Var
+def var_init(name: Token, initializer: Expr) -> Stmt:
+    assert isinstance(name, Token)
+    assert isinstance(initializer, Expr)
+
+    stmt = Stmt()
+    stmt.kind       = "Var"
+    stmt.name  =  name
+    stmt.initializer = initializer
+    return stmt
+
+def is_var(stmt: object) -> bool:
+    try: return stmt.kind == "Var"
+    except AttributeError: return False
+
+def var_to_str(var: Stmt) -> str:
+    name: str = var.name.lexeme
+    init: Expr = expr_to_str(var.initializer)
+    return f"Block()"
---
+++ eplox/test_exprs.py
@@ -0,0 +1,130 @@
+
+import pytest
+
+from exprs import *
+from tokens import *
+
+SOME_TOKEN = Token(TokenType.AND, "", None, 0)
+SOME_EXPR = Expr()
+
+def test_call_init():
+    call: Expr = call_init(SOME_EXPR, SOME_TOKEN, [SOME_EXPR])
+
+    if is_call(call) is False:
+        raise pytest.UsageError("'is_call': Expected to return True, but returned False.")
+
+    call_to_str(call)
+
+    expr: Expr = Expr()
+    if is_call(expr):
+        raise pytest.UsageError("'is_call': Expected to return False, but returned True.")
+
+    with pytest.raises(AssertionError):
+        is_call("Hello")
+
+
+def test_logical_init():
+    logical: Expr = logical_init(SOME_EXPR, SOME_TOKEN, SOME_EXPR)
+
+    if is_logical(logical) is False:
+        raise pytest.UsageError("'is_logical': Expected to return True, but returned False.")
+
+    logical_to_str(logical)
+
+    expr: Expr = Expr()
+    if is_logical(expr):
+        raise pytest.UsageError("'is_logical': Expected to return False, but returned True.")
+
+    with pytest.raises(AssertionError):
+        is_assign("Hello")
+
+
+def test_assign_init():
+    assign: Expr = assign_init(SOME_TOKEN, SOME_EXPR)
+
+    if is_assign(assign) is False:
+        raise pytest.UsageError("'is_assign': Expected to return True, but returned False.")
+
+    assign_to_str(assign)
+
+    expr: Expr = Expr()
+    if is_assign(expr):
+        raise pytest.UsageError("'is_assign': Expected to return False, but returned True.")
+
+    with pytest.raises(AssertionError):
+        is_assign("Hello")
+
+def test_variable_init():
+    variable: Expr = variable_init(SOME_TOKEN)
+
+    if is_variable(variable) is False:
+        raise pytest.UsageError("'is_variable': Expected to return True, but returned False.")
+
+    variable_to_str(variable)
+
+    expr: Expr = Expr()
+    if is_variable(expr):
+        raise pytest.UsageError("'is_variable': Expected to return False, but returned True.")
+
+    with pytest.raises(AssertionError):
+        is_variable("Hello")
+
+def test_grouping_init():
+    grouping: Expr = grouping_init(SOME_EXPR)
+
+    if is_grouping(grouping) is False:
+        raise pytest.UsageError("'is_grouping': Expected to return True, but returned False.")
+
+    grouping_to_str(grouping)
+
+    expr: Expr = Expr()
+    if is_grouping(expr):
+        raise pytest.UsageError("'is_grouping': Expected to return False, but returned True.")
+
+    with pytest.raises(AssertionError):
+        is_grouping("Hello")
+
+def test_unary_init():
+    unary: Expr = unary_init(SOME_TOKEN, SOME_EXPR)
+
+    if is_unary(unary) is False:
+        raise pytest.UsageError("'is_unary': Expected to return True, but returned False.")
+
+    unary_to_str(unary)
+
+    expr: Expr = Expr()
+    if is_unary(expr):
+        raise pytest.UsageError("'is_unary': Expected to return False, but returned True.")
+
+    with pytest.raises(AssertionError):
+        is_unary("Hello")
+
+def test_binop_init():
+    binop: Expr = binop_init(SOME_EXPR, SOME_TOKEN, SOME_EXPR)
+
+    if is_binop(binop) is False:
+        raise pytest.UsageError("'is_binop': Expected to return True, but returned False.")
+
+    binop_to_str(binop)
+
+    expr: Expr = Expr()
+    if is_binop(expr):
+        raise pytest.UsageError("'is_binop': Expected to return False, but returned True.")
+
+    with pytest.raises(AssertionError):
+        is_binop("Hello")
+
+def test_lit_init():
+    lit: Expr = lit_init(SOME_TOKEN)
+
+    if is_lit(lit) is False:
+        raise pytest.UsageError("'is_lit': Expected to return True, but returned False.")
+
+    lit_to_str(lit)
+
+    expr: Expr = Expr()
+    if is_lit(expr):
+        raise pytest.UsageError("'is_lit': Expected to return False, but returned True.")
+
+    with pytest.raises(AssertionError):
+        is_lit("Hello")
